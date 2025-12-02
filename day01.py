93fc24edb7ae6fa88b74e6dce99bfddfabbcbe30
inf6009 <alexander.hoernlein@uni-wuerzburg.de>
2025-12-02 15:46:59 +0100

    with types and better naming

--- day01.py
+++ day01.py
@@ -1,20 +1,23 @@
 import utils
 import os.path
-import math
-dir = os.path.dirname(__file__)
-ftest = os.path.join(dir, 'day01_test.txt')
-finput = os.path.join(dir, 'day01_input.txt')
+from math import floor
 
-def to_m_n(s):
+dir_ = os.path.dirname(__file__)
+ftest = os.path.join(dir_, 'day01_test.txt')
+finput = os.path.join(dir_, 'day01_input.txt')
+
+start_pos = 50
+
+def to_m_n(s: str) -> tuple[int, int]:
     # "Lxxx" or "Rxxx"
     m = -1 if s[0] == 'L' else +1 #R
     n = int(s[1:])
     return m, n
 
 @utils.timeit
-def part1(fname):
+def part1(fname: str) -> int:
     res = 0
-    pos = 50
+    pos = start_pos
     for s in utils.f2lines(fname):
         m, n = to_m_n(s)
         pos = (pos + m * n) % 100
@@ -27,16 +30,19 @@ def do1():
     assert 1089 == part1(finput)
 
 @utils.timeit
-def part2(fname):
+def part2(fname: str) -> int:
     res = 0
-    pos = 50
+    pos = start_pos
     for s in utils.f2lines(fname):
         m, n = to_m_n(s)
-        res += math.floor(n / 100) # multiple turns
-        pos_next = pos + m * (n % 100)
-        if pos_next > 100 or pos_next < 0 < pos: res += 1
+        res += floor(n / 100) # multiple turns
+        n %= 100
+        pos_next = pos + m * n
+        if pos_next > 100 or pos_next < 0 < pos: # went over 0
+            res += 1
         pos = pos_next % 100
-        if pos == 0: res += 1
+        if pos == 0:
+            res += 1
     return res
 
 def do2():
--- day02.py
+++ day02.py
@@ -1,31 +1,31 @@
 import utils
 import os.path
 from math import floor
-from utils import f2lines
+import typing
 
-dir = os.path.dirname(__file__)
-ftest = os.path.join(dir, 'day02_test.txt')
-finput = os.path.join(dir, 'day02_input.txt')
+dir_ = os.path.dirname(__file__)
+ftest = os.path.join(dir_, 'day02_test.txt')
+finput = os.path.join(dir_, 'day02_input.txt')
 
 @utils.timeit
-def part1(fname):
+def part1(fname: str) -> int:
     return part(fname, is_invalid_1)
 
-def part(fname, is_invalid):
+def part(fname: str, is_invalid: typing.Callable[[str], bool]) -> int:
     res = 0
-    for r in f2lines(fname)[0].split(','):
-        s, e = r.split('-')
-        for id in range(int(s), int(e) + 1):
-            if is_invalid(id):
-                res += id
+    # a-b,c-d,...
+    for r in utils.f2lines(fname)[0].split(','):
+        start, end = r.split('-')
+        for id_ in range(int(start), int(end) + 1):
+            if is_invalid(str(id_)):
+                res += id_
     return res
 
-def is_invalid_1(id):
-    s = str(id)
-    l = len(s)
+def is_invalid_1(id_: str) -> bool:
+    l = len(id_)
     if l % 2 == 0:
-        split = int(l / 2)
-        return s[:split] == s[split:]
+        half = int(l / 2)
+        return id_[:half] == id_[half:]
     return False
 
 def do1():
@@ -33,24 +33,24 @@ def do1():
     assert 12850231731 == part1(finput) # 0.7s
 
 @utils.timeit
-def part2(fname):
+def part2(fname: str) -> int:
     return part(fname, is_invalid_2)
 
-def is_invalid_2(id):
-    s = str(id)
-    l = len(s)
-    for pl in range(1, floor(l / 2) + 1):
-        if l % pl == 0:
-            all_eq = True
-            first = s[:pl]
-            for i in range(1, int(l / pl)):
-                if first != s[i * pl : (i + 1) * pl]:
-                    all_eq = False
-                    break
-            if all_eq:
+def is_invalid_2(id_: str) -> bool:
+    l = len(id_)
+    for part_length in range(1, floor(l / 2) + 1):
+        if l % part_length == 0:
+            if all_eq(id_, l, part_length):
                 return True
     return False
 
+def all_eq(id_: str, l: int, part_length: int) -> bool:
+    first_part = id_[:part_length]
+    for n in range(1, int(l / part_length)):
+        if first_part != id_[n * part_length : (n + 1) * part_length]:
+            return False
+    return True
+
 def do2():
     assert 4174379265 == part2(ftest)
-    assert 24774350322 == part2(finput) # 2.5s
+    assert 24774350322 == part2(finput) # 2.8s
