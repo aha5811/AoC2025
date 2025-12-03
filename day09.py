7a397b657a64af64fdeca0c6e0b161d86fef1477
inf6009 <alexander.hoernlein@uni-wuerzburg.de>
2025-12-03 10:17:39 +0100

    prepared day04 - day12

---
+++ day09.py
@@ -0,0 +1,26 @@
+import utils
+import os.path
+
+dir_ = os.path.dirname(__file__)
+ftest = os.path.join(dir_, 'day09_test.txt')
+finput = os.path.join(dir_, 'day09_input.txt')
+
+@utils.timeit
+def part1(fname: str) -> int:
+    res = 0
+    # TODO
+    return res
+
+def do1():
+    assert 0 == part1(ftest)
+    assert 0 == part1(finput)
+
+@utils.timeit
+def part2(fname: str) -> int:
+    res = 0
+    # TODO
+    return res
+
+def do2():
+    assert 0 == part2(ftest)
+    assert 0 == part2(finput)
