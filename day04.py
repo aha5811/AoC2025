eb8abb33764f52affdea7b8bd6617d3cbcb43a4f
inf6009 <alexander.hoernlein@uni-wuerzburg.de>
2025-12-04 12:23:32 +0100

    day 4 part 2 speedup + refactoring

--- day04.py
+++ day04.py
@@ -6,18 +6,24 @@ dir_ = os.path.dirname(__file__)
 ftest = os.path.join(dir_, 'day04_test.txt')
 finput = os.path.join(dir_, 'day04_input.txt')
 
+rop = '@' # roll of paper
+rem = 'x' # removed
+
 @utils.timeit
 def part1(fname: str) -> int:
     res = 0
     m = maps.Map(fname)
-    for p in m.find_all('@'):
-        cnt = 0
-        for d in maps.ds:
-            if m.get(p.x + d[0], p.y + d[1]) == '@':
-                cnt += 1
-        if cnt < 4:
+    for p in m.find_all(rop):
+        if accessible(m, p):
             res += 1
     return res
+
+def accessible(m: maps.Map, p: maps.Pos) -> bool:
+    res = 0
+    for d in maps.ds: # all 8
+        if m.get(p.x + d[0], p.y + d[1]) == rop:
+            res += 1
+    return res < 4
 
 def do1():
     assert 13 == part1(ftest)
@@ -26,21 +32,20 @@ def do1():
 @utils.timeit
 def part2(fname: str) -> int:
     m = maps.Map(fname)
+    ps = set(m.find_all(rop))
+    res = 0
     while True:
-        ps = []
-        for p in m.find_all('@'):
-            cnt = 0
-            for d in maps.ds:
-                if m.get(p.x + d[0], p.y + d[1]) == '@':
-                    cnt += 1
-            if cnt < 4:
-                ps.append(p)
-        if len(ps) == 0:
-            break
+        remove = set()
         for p in ps:
-            m.set(p.x, p.y, 'x')
-    return len(m.find_all('x'))
+            if accessible(m, p):
+                m.set(p.x, p.y, rem)
+                res += 1
+                remove.add(p)
+        if not remove:
+            break
+        ps -= remove
+    return res
 
 def do2():
     assert 43 == part2(ftest)
-    assert 9290 == part2(finput) # 1s
+    assert 9290 == part2(finput) # 0.4s
