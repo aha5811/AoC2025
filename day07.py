c1b74ba6e94d52a0a58f4c346ad14ff16acbc847
inf6009 <alexander.hoernlein@uni-wuerzburg.de>
2025-12-08 13:15:15 +0100

    day 7

--- day07.py
+++ day07.py
@@ -1,4 +1,5 @@
 import utils
+from maps import Map, Pos
 import os.path
 
 dir_ = os.path.dirname(__file__)
@@ -8,19 +9,74 @@ finput = os.path.join(dir_, 'day07_input.txt')
 @utils.timeit
 def part1(fname: str) -> int:
     res = 0
-    # TODO
+
+    m = Map(fname)
+    # it's always one line with S or ^ then an empty line - but we don't use that
+
+    ts = m.find_all('S')
+    while True:
+        next_y = ts[0].y + 1
+
+        if m.get(0, next_y) is None: break
+
+        ts_next = []
+        for t in ts:
+            if m.get(t.x, next_y) == '^':
+                res += 1
+                add(ts_next, t.x - 1, next_y)
+                add(ts_next, t.x + 1, next_y)
+            else:
+                add(ts_next, t.x, next_y)
+
+        ts = ts_next
+
     return res
 
+def add(ps: list[Pos], x: int, y: int):
+    p = Pos(x, y)
+    if p not in ps:
+        ps.append(p)
+
 def do1():
-    assert 0 == part1(ftest)
-    assert 0 == part1(finput)
+    assert 21 == part1(ftest)
+    assert 1590 == part1(finput)
 
 @utils.timeit
 def part2(fname: str) -> int:
-    res = 0
-    # TODO
-    return res
+
+    m = Map(fname)
+
+    ts = [(m.find_all('S')[0], 1)]
+
+    while True:
+        next_y = ts[0][0].y + 1
+
+        if m.get(0, next_y) is None: break
+
+        ts_next = []
+        for t in ts:
+            (p, cnt) = t
+            if m.get(p.x, next_y) == '^':
+                add2(ts_next, p.x - 1, next_y, cnt)
+                add2(ts_next, p.x + 1, next_y, cnt)
+            else:
+                add2(ts_next, p.x, next_y, cnt)
+
+        ts = ts_next
+
+    return sum(map(lambda t: t[1], ts))
+
+def add2(ts: list[tuple[Pos, int]], x: int, y: int, cnt: int):
+    p = Pos(x, y)
+    found = False
+    for i in range(len(ts)):
+        t = ts[i]
+        if t[0] == p:
+            ts[i] = (p, t[1] + cnt)
+            found = True
+    if not found:
+        ts.append((p, cnt))
 
 def do2():
-    assert 0 == part2(ftest)
-    assert 0 == part2(finput)
+    assert 40 == part2(ftest)
+    assert 20571740188555 == part2(finput)
