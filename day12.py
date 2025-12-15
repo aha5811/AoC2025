a0944fe523da74d771aa2bdb2a26572c48d68b4b
inf6009 <alexander.hoernlein@uni-wuerzburg.de>
2025-12-15 10:48:48 +0100

    day 12 problem reading/preprocessing - but no solver

--- day12.py
+++ day12.py
@@ -26,11 +26,15 @@ def part1(fname: str) -> int:
             dims, rest = l.split(':')
             (w, h) = tuple(map(int, dims.split('x')))
             area = [[False for _ in range(h)] for _ in range(w)]
-            shape_amnts = []
+            shapes2fit = []
             for n, amnt in enumerate(utils.s2ns(rest)):
-                shape_amnts += [n] * amnt
-            res += solve(shape_dict, shape_amnts, area)
+                shapes2fit += [n] * amnt
+            is_solvable = solve(shape_dict, shapes2fit, area)
+            print(l, is_solvable)
+            if is_solvable:
+                res += 1
 
+    print(res)
     return res
 
 def gen_variants(lines: list[str]):
@@ -68,32 +72,62 @@ def rotate_right(shape: list[list[bool]]) -> list[list[bool]]:
 def flip_h(shape: list[list]) -> list[list]:
     return list(map(lambda l: list(reversed(l)), shape))
 
+# - iterate over the shape numbers
+# -- iterate over possible shape variants in dict
+# --- iterate over x of area
+# ---- iterate over y of area
+# ----- if fits: place + recursion
+
 def solve(
         shape_dict: dict[int, list[list[list[bool]]]],
-        shape_amnts: list[int],
+        shapes2fit: list[int],
         area: list[list[bool]]) -> bool:
-
-    # - iterate over the shape numbers
-    # -- iterate over possible shapes in dict
-    # --- iterate over x of area
-    # ---- iterate over y of area
-    # ----- if fits: place + recursion
-
+    if len(shapes2fit) == 0:
+        return True
+    for p, n in enumerate(shapes2fit):
+        if solve_s2f(shape_dict, shapes2fit, p, n, area):
+            return True
     return False
 
-def fits(shape, x, y, area) -> bool:
+def solve_s2f(shape_dict, shapes2fit: list[int], p: int, n: int, area: list[list[bool]]) -> bool:
+    for sh in shape_dict[n]:
+        if solve_sh(shape_dict, shapes2fit, p, sh, area):
+            return True
+    return False
+
+def solve_sh(shape_dict, shapes2fit: list[int], p: int, shape: list[list[bool]], area: list[list[bool]]) -> bool:
+    for x in range(len(area)):
+        if solve_x(shape_dict, shapes2fit, p, shape, area, x):
+            return True
+    return False
+
+def solve_x(shape_dict, shapes2fit: list[int], p:int, shape: list[list[bool]], area: list[list[bool]], x: int) -> bool:
+    for y in range(len(area[0])):
+        if fits(shape, x, y, area):
+            return solve(shape_dict, dec_shape(shapes2fit, p), place(shape, x, y, area))
+    return False
+
+def fits(shape: list[list[bool]], x: int, y: int, area: list[list[bool]]) -> bool:
+    if x + 2 >= len(area) or y + 2 >= len(area[0]):
+        return False
     for xx in range(3):
         for yy in range(3):
-            if shape[xx][yy] and area[x + xx][y + yy]:
+             if shape[xx][yy] and area[x + xx][y + yy]:
                 return False
     return True
 
-def place(shape, x, y, area):
+def place(shape: list[list[bool]], x: int, y: int, area: list[list[bool]]) -> list[list[bool]]:
+    print('placing', shape, 'at', x, y)
     area_next = area.copy()
     for xx in range(3):
         for yy in range(3):
             area[x + xx][y + yy] = shape[xx][yy]
     return area_next
+
+def dec_shape(shapes2fit: list[int], p: int) -> list[int]:
+    ret = shapes2fit.copy()
+    ret.pop(p)
+    return ret
 
 def do1():
     assert 2 == part1(ftest)
